66c9ba5a774e40f1831876c51cc7dcdfcd7e26e3
Lucaz97 <lc4976@nyu.edu>
2025-02-25 22:54:08 -0500

    increased tokens for hyperbolic as includes reasoning tokens

--- src/c2hlsc.py
+++ src/c2hlsc.py
@@ -353,17 +353,17 @@ def call_llm(model, message_list, cfg):  # unified interface for calling differe
         return message.content[0].text
     else: 
         try:
+            max_tokens = 131072 if "hyperbolic" in model else 8192
             completion = cfg.client.chat.completions.create(
                 model=model,
                 messages = message_list,
-                max_tokens=8192
+                max_tokens=max_tokens
                 #top_p=0.2,
                 #temperature=0.25
             )
         except Exception as e:
             if "Expecting value:" in str(e) and llm_api_errors < 5:
                 print("API unavailable, retrying in a minute")
-                time.sleep(60)
                 llm_api_errors += 1
                 return call_llm(model, message_list, cfg)
             
@@ -374,6 +374,11 @@ def call_llm(model, message_list, cfg):  # unified interface for calling differe
         llm_api_errors = 0
         print("LLM RAW RESPONSE: ", completion)
         if "hyperbolic" in cfg.model and "reasoner" in cfg.model:
+            if "<think>" not in completion.choices[0].message.content:
+                print("Too many thinking tokens .-., reasoning did not fit in the max tokens")
+                time.sleep(60)
+                llm_api_errors += 1
+                return call_llm(model, message_list, cfg)
             # need to filter out the thinking tokens: <think> thinking tokens <think/>
             content = completion.choices[0].message.content
             content = content.split("</think>")[1]
