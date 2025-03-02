85b217a25f9012d27f164fd5e110f9bb638b19a4
ajh9498 <ajh9498@hansolo.poly.edu>
2025-03-01 22:43:20 -0500

    adding output directories

---
+++ keep_outputs/outputs_filter_deepseek-chat_7/Catapult_filter/filter.v1/res_sharing.tcl
@@ -0,0 +1,5 @@
+directive set /filter/filter:core/filter:core:conc/fir:for#1:acc RESOURCE_NAME fir:for#1:acc:rg
+directive set /filter/filter:core/filter:core:conc/iir:for#1:acc#1 RESOURCE_NAME fir:for#1:acc:rg
+directive set /filter/filter:core/filter:core:conc/iir:iir:acc#1 RESOURCE_NAME fir:for#1:acc:rg
+directive set /filter/filter:core/filter:core:conc/fir:for#1-5:acc#1 RESOURCE_NAME fir:for#1-5:acc#1:rg
+directive set /filter/filter:core/filter:core:conc/iir:for#2:acc#1 RESOURCE_NAME fir:for#1-5:acc#1:rg
---
+++ keep_outputs/outputs_filter_o3-mini_6/Catapult_filter/filter.v1/cycle_set.tcl
@@ -0,0 +1,27 @@
+
+# Loop constraints
+directive set /filter/core/core:rlp CSTEPS_FROM {{. == 0}}
+directive set /filter/core/core:rlp/main CSTEPS_FROM {{. == 2} {.. == 0}}
+
+# IO operation constraints
+directive set /filter/core/core:rlp/main/input:io_read(input:rsc.@) CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/fir:io_write(fir_output:rsc.@) CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/iir:io_write(iir_output:rsc.@) CSTEPS_FROM {{.. == 1}}
+
+# Sync operation constraints
+
+# Real operation constraints
+directive set /filter/core/core:rlp/main/fir:p2:acc#1 CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/fir:p2:acc#3 CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/fir:p2:acc CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/fir:p2:acc#2 CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/fir:sum:acc CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/iir:feedforward:acc#6 CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/iir:feedforward:acc#5 CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/iir:feedforward:acc#8 CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/iir:feedforward:acc#4 CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/iir:feedforward:acc#7 CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/iir:feedforward:acc CSTEPS_FROM {{.. == 1}}
+directive set /filter/core/core:rlp/main/iir:iir:acc#1 CSTEPS_FROM {{.. == 1}}
+
+# Probe constraints
