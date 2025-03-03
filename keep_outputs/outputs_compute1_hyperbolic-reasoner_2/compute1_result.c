df11f917c60589d27680ce31efbcbbb428cf08df
Lucaz97 <lc4976@nyu.edu>
2025-03-02 22:37:09 -0500

    added results from batch 7

---
+++ keep_outputs/outputs_compute1_hyperbolic-reasoner_2/compute1_result.c
@@ -0,0 +1,44 @@
+
+#include "../include/ac_float.h"
+#include "../include/ac_fixed.h"
+#include <stdint.h>
+#include <stdio.h>
+
+int odd_factorial(int n) {
+  int res = 1;
+  #pragma HLS unroll yes
+  for (int i = 1; i < n; i += 2) {
+    res = res * i;
+  }
+  return res;
+}
+
+int even_sum(int n)
+{
+  int sum = 0;
+  #pragma hls_unroll yes
+  for (int i = 2; i < n; i += 2)
+  {
+    sum += i;
+  }
+  return sum;
+}
+
+int compute1(int n)
+{
+  #pragma HLS INLINE
+  int result;
+  result = odd_factorial(n);
+  result = even_sum(result);
+  return result;
+}
+int main()
+{
+  int n = 0xb;
+  int ret;
+  ret = compute1((int) n);
+  printf("%d\n", ret);
+  printf("%d\n", n);
+}
+
+
